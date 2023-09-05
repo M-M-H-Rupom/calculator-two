e3d63766d4aaddd286fcc79a42a15793fa684112
rupom <mohrajulrupom@gmail.com>
2023-09-05 13:01:09 -0400

    new

--- script.js
+++ script.js
@@ -6,13 +6,24 @@ const display= (value) => {
 const clearall = () => {
     document.getElementById("result").value = ""
  }
-const calculate = () => {
-    var a = document.getElementById("result").value;
-    var b = eval(a);
-    document.getElementById("result").value = b;
+ const calculate = () => {
+    const input = document.getElementById("result").value;
+    
+    try {
+        const results = eval(input);
+        if (typeof results === 'number' && isFinite(results)) {
+            document.getElementById("result").value = results;
+        } else {
+            document.getElementById("result").value = "Error";
+        }
+    } catch (error) {
+        document.getElementById("result").value = "Error";
+    }
 }
+
 const deleteprevious = () => {
     var c = document.getElementById("result").value;
     var d = c.toString().slice(0,-1);
     document.getElementById("result").value = d;
 }
+
